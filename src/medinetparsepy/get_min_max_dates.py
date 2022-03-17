912f62ae15c00366216607091859a0a379c3395a
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-03-17 20:29:17 +0100

    Added plot_shift_type_by_frequency function.

--- src/medinetparsepy/get_min_max_dates.py
+++ src/medinetparsepy/get_min_max_dates.py
@@ -11,7 +11,7 @@ def get_min_max_dates(tidy_schedule: pandas.DataFrame) -> tuple:
     :param tidy_schedule: A schedule as returned by load_tidy_schedule().
     :type tidy_schedule: pandas.DataFrame
     :return: A tuple where the first value is the first date in the schedule
-        and the last value is the last date in the schedule.
+    and the last value is the last date in the schedule.
     :rtype: tuple
     """
     return (tidy_schedule['date'].min().strftime('%Y-%m-%d'),
